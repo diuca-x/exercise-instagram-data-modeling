3e3901fff392f102831397d194896b03fffd2f9a
diuca puig <diucapuig@gmail.com>
2023-05-28 18:30:52 +0000

    done

--- src/models.py
+++ src/models.py
@@ -1,32 +1,57 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = "user"
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    nombre = Column(String(100), nullable =False)
+    dni = Column(Integer)
+    nac = Column(Integer)
+    tel = Column(Integer)
+    email = Column(String(100),nullable = False)
+    password = Column(String(100), nullable = False)
+    identificador = Column(String(100), nullable= False)
 
-    def to_dict(self):
-        return {}
+class Comentarios (Base):
+    __tablename__ = "comentarios"
+    id = Column(Integer, primary_key=True)
+    text = Column(String(1000), nullable = False)
+    user_id = Column(Integer, ForeignKey("User.id"))
+    publi_id = Column(Integer, ForeignKey("Publicaciones.id"))
+
+class Publicaciones(Base):
+    __tablename__ = "publicaciones"
+    id = Column(Integer, primary_key=True)
+    src = Column(String(1000), nullable = False)
+    descripcion = Column(String(420))
+    user_id = Column(Integer, ForeignKey("User.id"))
+
+class Historias (Base):
+    __tablename__ = "historias"
+    id = Column(Integer, primary_key=True)
+    src = Column(String(1000), nullable = False) 
+    user_id = Column(Integer, ForeignKey("User.id"))
+
+class Notificaciones(Base):
+    __tablename__ = "notificaciones"
+    id = Column(Integer, primary_key=True)
+    id_origen = Column(Integer, nullable=False)
+    id_destino = Column(Integer, nullable=False)
+    tipo = Column(String,nullable = False) #si es por una etiquetacion, seguidor, like etc
+
+class Follower(Base): 
+    __tablename__ = "follower"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("User.id"))
+    te_sigue = Column(Boolean, nullable = False)
+    lo_sigues = Column(Boolean, nullable = False)
 
 ## Draw from SQLAlchemy base
 try:
